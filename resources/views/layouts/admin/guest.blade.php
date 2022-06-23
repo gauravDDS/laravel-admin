9d207c0176ec273c8f762ce7302a99e3badddb91
Gaurav <gaurav@dreamzdigitalsolutions.com>
2022-06-23 15:31:50 +0530

    laravel-admin-auth

---
+++ resources/views/layouts/admin/guest.blade.php
@@ -0,0 +1,22 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>    
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>{{ config('app.name', 'Laravel') }}</title>    
+    <!-- Custom fonts for this template-->
+    <link href="{{ asset('assets/fontawesome/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('assets/css/sb-admin-2.min.css') }}" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
